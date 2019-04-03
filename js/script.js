948ec75cd0e8e302b2ef2a7f0574b019f3f37a8a
coolguybhargav <44438713+bhargavgana@users.noreply.github.com>
2019-04-04 00:02:26 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function(){
+	$("#about-me").typed({
+		strings: ["Student at Amrita University.","Open Source Enthusiast","Web designer","Analytical | loyal |Techie.","innovative coder<>."],
+		typeSpeed: 30,
+		loop: true,
+	});
+});
