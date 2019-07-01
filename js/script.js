7fe3753174f42e5306e000bc393dc52be98773f6
coolguybhargav <44438713+bhargavgana@users.noreply.github.com>
2019-07-02 00:59:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function(){
 	$("#about-me").typed({
-		strings: ["Student at Amrita University.","Open Source Enthusiast","Web designer","Analytical | loyal |Techie."]
+		strings: ["Sophomore at Amrita University.","Open Source Enthusiast","Web designer","Analytical | loyal |Techie."]
 		typeSpeed: 30,
 		loop: true,
 	});
